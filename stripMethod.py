a3237c1f99969868a6c7cd6cf134f393bb16ed3a
Elijah Herald <elijah.herald@gmail.com>
2025-03-13 08:05:18 -0400

    created a regex that mimics the strip method

--- stripMethod.py
+++ stripMethod.py
@@ -1,6 +1,18 @@
+# takes a string and mimics the strip() method
+# if no other arguments are passed other than the string to strip
+# then whitespace characters are removed from beginning and end of string
+# otherwise the characters specified in the second argument to the function will be removed from the string
+
 import re
 
-stripRegex = re.compile(r'\s')
+# stripRegex = re.compile(r'\s')
 
-mo = stripRegex.sub('', input('Please enter string: '))
-print(mo)
+# mo = stripRegex.sub('', input('Please enter string: '))
+# print(mo)
+
+def stripMimic(str, char=' '):
+    stripRegex = re.compile(f'{char}')
+    strippedStr = stripRegex.sub('',str)
+    return strippedStr
+
+print(stripMimic(input('Please enter string: '),'f'))
